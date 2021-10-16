7b22ccf64e47342c39e3c1dc31bffd113e2ae99b
whoward2929 <Whoward939@gmail.com>
2021-10-15 21:20:54 -0400

    added controllers

---
+++ controllers/thought-controllers.js
@@ -0,0 +1,35 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+    //get all thoughts
+    getAllThought(req, res) {
+
+    },
+
+    //get one thought by id
+    getThoughtById({ params }, res) {
+
+    },
+
+    //create a thought
+    createThought({ params, body }, res) {
+
+    },
+
+    //delete a thought
+    deleteThought({ params }, res) {
+
+    },
+
+    //add a reaction
+    addReaction({ params, body}, res) {
+
+    },
+
+    //remove reaction
+    removeReaction({ params }, res) {
+
+    }
+
+    
+}
---
+++ controllers/user-controllers.js
@@ -0,0 +1,29 @@
+const { User } = require('../models');
+
+const userController = {
+    //get all users
+    getAllUsers(req, res) {
+        
+    },
+
+    //get one user by id
+    getUserById({ params }, res) {
+
+    },
+
+    //create new user
+    createUser({ body }, res) {
+
+    },
+
+    //update a user
+    updateUser({ params, body}, res) {
+
+    },
+
+    //delete user
+    deleteUser({ params }, res) {
+        
+    }
+
+}
