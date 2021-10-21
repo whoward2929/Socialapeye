bd19b4d494bf90dfd0333259bfafc8719e2c32f0
whoward2929 <Whoward939@gmail.com>
2021-10-21 14:22:21 -0400

    routes setup

---
+++ routes/api/thoughts-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+const {
+    getAllThought,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought
+  } = require('../../controllers/thought-controller');
+
+ 
+router
+  .route('/')
+  .get(getAllThought)
+  .post(createThought);
+
+ 
+router
+  .route('/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+module.exports = router;
+
+
